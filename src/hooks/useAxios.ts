40f360dd06c01e8483f9eff42a59a8112b76a2b4
Joao Dorino <js9978853@gmail.com>
2026-02-22 16:46:00 -0300

    feat: salvando progresso antes de mudar de branch

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -1,7 +1,9 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import type { AxiosRequestConfig } from "axios";
+import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
@@ -34,4 +36,41 @@ export const usePost = <T, P>(endpoint: string) => {
   };
 
   return { data, loading, error, postData };
+};
+
+
+export const useGet = <T>( endpoint: string, config?: AxiosRequestConfig) => {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<number | null>(null);
+
+  const getData = async () => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await axiosInstance({
+        url: endpoint,
+        method: 'GET',
+        headers: {
+          "Authorization": `Bearer ${Cookies.get('Authorization')}`,
+         ...config?.headers,
+        },
+        ...config,
+      });
+
+      setData(response.data);
+    } catch (err: any) {
+      setError(err?.response?.status ?? 500);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+useEffect(() => {
+  getData()
+}, [])
+
+
+  return { data, loading, error, getData };
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,13 @@
- import { AvatarList ,CardComponent, CustomChart, CustomTable, Header } from "@/components"
- import { Container } from "@mui/material"
- import { currencyConverter } from "@/utils"
+import {
+  AvatarList,
+  CardComponent,
+  CustomChart,
+  CustomTable,
+  Header,
+  StyledH2,
+} from '@/components'
+import { Container, Grid } from '@mui/material'
+import { currencyConverter } from '@/utils'
 
 function Home() {
   const mockListData = [
@@ -19,7 +26,6 @@ function Home() {
       name: 'Name Sobrenome 3',
       subtitle: currencyConverter(3000.03),
     },
-
   ]
 
   const mockTableData = {
@@ -28,44 +34,78 @@ function Home() {
       [
         <span>Nome 1</span>,
         <span>Nome1@gmail.com</span>,
-        <button>ACTION</button>
+        <button>ACTION</button>,
       ],
       [
         <span>Nome 2</span>,
         <span>Nome2@gmail.com</span>,
-        <button>ACTION</button>
+        <button>ACTION</button>,
       ],
       [
         <span>Nome 3</span>,
         <span>Nome3@gmail.com</span>,
-        <button>ACTION</button>
-      ]
-    ]
-
+        <button>ACTION</button>,
+      ],
+    ],
   }
 
   return (
-    <> 
-    <Header />
-    <Container maxWidth="lg">
-      <CardComponent >
-        CARD
-      </CardComponent>
-      <CardComponent >
-        <AvatarList listData={mockListData} />
-      </CardComponent>
-      <CardComponent >
-        <CustomTable headers={mockTableData.headers}
-        rows={mockTableData.rows} />
-      </CardComponent>
-      <CardComponent>
-        <CustomChart
-         labels={['Jan','Fev','Mar','Abr','Mai']}
-         data={[1000.12, 2456.54, 986.32, 654.89, 754.89, 354.89  ]}
-         type="line" />
-      </CardComponent>
-    </Container>
-    
+    <>
+      <Header />
+      <Container maxWidth="lg">
+        <Grid container spacing={4}>
+          <Grid item xs={12} md={4}>
+            <CardComponent>
+              <StyledH2 className="mb-1">Total de vendas no mês</StyledH2>
+            </CardComponent>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <CardComponent>
+              <StyledH2 className="mb-1">Meta do mês</StyledH2>
+            </CardComponent>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <CardComponent>
+              <StyledH2 className="mb-1">Leads contactados</StyledH2>
+            </CardComponent>
+          </Grid>
+          <Grid item xs={12} md={7}>
+            <CardComponent>
+              <StyledH2 className="mb-1">Valor de vendas no mês</StyledH2>
+              <CustomChart
+                labels={['Jan', 'Fev', 'Mar', 'Abr', 'Mai']}
+                data={[1000.12, 2456.54, 986.32, 654.89, 754.89, 354.89]}
+                type="line"
+              />
+            </CardComponent>
+          </Grid>
+          <Grid item xs={12} md={5}>
+            <CardComponent>
+              <StyledH2 className="mb-1">Maiores vendedores do mês</StyledH2>
+              <AvatarList listData={mockListData} />
+            </CardComponent>
+          </Grid>
+          <Grid item xs={12} md={5}>
+            <CardComponent>
+              <StyledH2 className="mb-1">Notícias relevantes</StyledH2>
+              <CustomTable
+                headers={mockTableData.headers}
+                rows={mockTableData.rows}
+              />
+            </CardComponent>
+          </Grid>
+          <Grid item xs={12} md={7}>
+            <CardComponent>
+              <StyledH2 className="mb-1">Valor de vendas por mês</StyledH2>
+              <CustomChart
+                labels={['Jan', 'Fev', 'Mar', 'Abr', 'Mai']}
+                data={[1000.12, 2456.54, 986.32, 654.89, 754.89, 354.89]}
+                type="bar"
+              />
+            </CardComponent>
+          </Grid>
+        </Grid>
+      </Container>
     </>
   )
 }
